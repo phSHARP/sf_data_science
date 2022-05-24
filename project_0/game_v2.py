c4357da0bb18861047f1d0ea5ea71bbf4f239fc4
phSHARP <konstant-vm@mail.ru>
2022-05-24 17:53:21 +0300

    algorithm optimization

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -12,13 +12,18 @@ def random_predict(number:int=1) -> int:
     '''
     
     count = 0
+    left_border, right_border = 1, 101
     
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # Estimated number
-        if number == predict_number:
+        predict_number = np.random.randint(left_border, right_border)  # Estimated number
+        if predict_number < number:
+            left_border = predict_number + 1  # Exclude predicted number
+        elif predict_number > number:
+            right_border = predict_number     # Exclude predicted number (by not adding 1)
+        else:
             break  # End cycle if guessed succesfully
-    return(count)
+    return count
 
 def score_game(random_predict:int) -> int:
     '''
@@ -33,15 +38,16 @@ def score_game(random_predict:int) -> int:
     
     count_ls = []  # List for saving the number of attempts
     np.random.seed(1)  # Fixing the seed for the result consistency
-    random_array = np.random.randint(1, 101, size=(1000))  # List of guessed numbers
+    random_array = np.random.randint(1, 101, size=1000)  # List of guessed numbers
     
     for number in random_array:
         count_ls.append(random_predict(number))
     
+    np.random.seed()  # Unfixing the seed in order to avoid side effects
     score = int(np.mean(count_ls))  # Average number of attempts
     
     print(f'Your algorithm guesses a number in {score} attempts on average.')
-    return(score)
+    return score
 
 # RUN
 if __name__ == '__main__':
